1f6433e393252eacb34a2964c228c16c97083f87
samzimmerman95 <samzimmerman95@gmail.com>
2023-03-13 20:02:46 -0700

    Transmitter update

--- Transmitter/src/main.cpp
+++ Transmitter/src/main.cpp
@@ -3,16 +3,24 @@
 // #include "printf.h"
 #include <nRF24L01.h>
 #include "RF24.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 14
+#define NUM_SENSORS 2
+#define PWR_CONTROLLER_DONE 22
 
 // instantiate an object for the nRF24L01 transceiver
 RF24 radio(4, 5); // CE pin, CSN pin
-
 uint8_t address[][6] = {"1Node"};
 
-// For this example, we'll be using a payload containing
-// a single float number that will be incremented
-// on every successful transmission
-float payload = 0.0;
+// Init for temp sensors
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+int deviceCount = 0;
+float Celcius = 0;
+float Fahrenheit = 0;
+float tempList[NUM_SENSORS] = {};
 
 void setup()
 {
@@ -21,39 +29,63 @@ void setup()
   delay(1000);
 
   Serial.println("Transmitter Starting");
-
-  // initialize the transceiver on the SPI bus
+  pinMode(PWR_CONTROLLER_DONE, OUTPUT);
   if (!radio.begin())
   {
     while (1)
     {
       Serial.println("radio hardware is not responding!!");
       delay(1000);
-    } // hold in infinite loop
+    }
   }
 
-  radio.setPALevel(RF24_PA_LOW); // RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_MAX); // RF24_PA_MAX is default.
+  radio.setDataRate(RF24_250KBPS);
+  radio.setChannel(115);
 
-  // save on transmission time by setting the radio to only transmit the
-  // number of bytes we need to transmit a float
-  radio.setPayloadSize(sizeof(payload)); // float datatype occupies 4 bytes
-  radio.setRetries(15, 15);              // delay, count
+  // save on transmission time by setting the radio to only transmit the size we need
+  radio.setPayloadSize(sizeof(tempList));
+  radio.setRetries(15, 15); // delay, count
 
   radio.openWritingPipe(address[0]); // always uses pipe 0
 
   // For debugging info
-  // printf_begin();             // needed only once for printing details
   radio.printPrettyDetails(); // (larger) function that prints human readable data
+
+  // Setup for temp sensors
+  sensors.begin();
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  deviceCount = sensors.getDeviceCount();
+  Serial.print(deviceCount);
+  Serial.println(" devices.");
+  Serial.println("");
 
 } // setup
 
+void updateTemp()
+{
+
+  delay(1000);
+  sensors.requestTemperatures();
+  delay(1000);
+
+  for (int i = 0; i < deviceCount; i++)
+  {
+    Celcius = sensors.getTempCByIndex(i);
+    Fahrenheit = sensors.toFahrenheit(Celcius);
+    tempList[i] = Fahrenheit;
+  }
+}
+
 void loop()
 {
+  updateTemp();
   // This device is a TX node
 
-  unsigned long start_timer = micros();               // start the timer
-  bool report = radio.write(&payload, sizeof(float)); // transmit & save the report
-  unsigned long end_timer = micros();                 // end the timer
+  unsigned long start_timer = micros();                   // start the timer
+  bool report = radio.write(&tempList, sizeof(tempList)); // transmit & save the report
+  unsigned long end_timer = micros();                     // end the timer
   delay(500);
 
   if (report)
@@ -62,14 +94,20 @@ void loop()
     Serial.print("Time to transmit = ");
     Serial.print(end_timer - start_timer); // print the timer result
     Serial.print(" us. Sent: ");
-    Serial.println(payload); // print payload sent
-    payload += 0.01;         // increment float payload
+    for (int i = 0; i < deviceCount; i++)
+    {
+      Serial.print(tempList[i]);
+      Serial.print(" ");
+    }
+    Serial.println("");
   }
   else
   {
     Serial.println("Transmission failed or timed out"); // payload was not delivered
   }
 
-  delay(5000); // slow transmissions down by 1 second
+  delay(5000);
+  digitalWrite(PWR_CONTROLLER_DONE, HIGH);
+  // delay(5000); // slow transmissions down
 
 } // loop
